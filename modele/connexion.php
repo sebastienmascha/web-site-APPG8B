1383491b7596813470ccfefe9a3fda8c6ded3956
seb <sebastien.mascha@icloud.com>
2019-03-04 12:17:29 +0100

    BDD hebergee + Ajout controleur admin

--- modele/connexion.php
+++ modele/connexion.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-    $bdd = new PDO('mysql:host=localhost:8889;dbname=mvc;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $bdd = new PDO('mysql:host=db4free.net:3306;dbname=mvcg8b;charset=utf8', 'appg8b', 'jesuislemotdepasseg8b', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 catch(Exception $e)
 {
