e1a720bf817f2717cb2f7d8749c004c1c76a41ab
seb <sebastien.mascha@icloud.com>
2019-03-04 14:26:46 +0100

    Nouveau hebergement pour le SQL
    Chez : freesqldatabase

--- modele/connexion.php
+++ modele/connexion.php
@@ -1,9 +1,11 @@
 <?php
 
 try{
-    $bdd = new PDO('mysql:host=db4free.net:3306;dbname=mvcg8b;charset=utf8', 'appg8b', 'jesuislemotdepasseg8b', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $bdd = new PDO('mysql:host=sql7.freesqldatabase.com:3306;dbname=sql7281782;charset=utf8', 'sql7281782', 'W2ZuBw3MyN', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 catch(Exception $e)
 {
     die('Erreur : '.$e->getMessage());
 }
+
+// $bdd = new PDO('mysql:host=db4free.net:3306;dbname=mvcg8b;charset=utf8', 'appg8b', 'jesuislemotdepasseg8b', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
