a15a89ea6041f9bca93e3f79f45ebeac61884c11
Augustin Simon <augussim@gmail.com>
2019-03-13 11:54:16 +0100

    [Stock] Mise en place du stock

--- vues/stock.php
+++ vues/stock.php
@@ -1,19 +1,64 @@
-<?php 
-/**
-* Vue : accueil
-*/
-?>
+<?php  /** * Vue : accueil */ ?>
 
 <style>
 <?php include "css/css_stock.css"; ?>
 </style>
 
 
+
+
+
+
+
 <div class="boite">
-    <p>Lee Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
-    </p>
+
+<p> Café </p>
+<div class="box">
+	<button> Commander </button>
 </div>
+<br>
+<div class="progress-bar green"></div>
+</div>
+
+
+
+
 <div class="boite">
-    <p>n et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
-    </p>
+
+<p> Chocolat chaud </p>
+<div class="box">
+	<button> Commander </button>
+</div>
+<br>
+    <div class="progress-bar green"></div>
+</div>
+
+
+
+
+
+
+<div class="boite">
+
+<p> Expresso </p>	
+<div class="box">
+	<button> Commander </button>
+</div>
+<br>
+    <div class="progress-bar green"></div>
+</div>
+
+
+
+
+
+
+<div class="boite">
+
+<p> Thé </p>
+<div class="box">
+	<button> Commander </button>
+</div>
+<br>
+<div class="progress-bar green"></div>
 </div> 
