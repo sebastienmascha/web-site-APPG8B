4b1f191e0137e7293a1c4f762252c6d5b128c439
Agathe Le Bris <agathelebris98@gmail.com>
2019-03-18 15:30:15 +0100

    Avancement page accueil
    html accueil et css en cours

--- vues/accueil.php
+++ vues/accueil.php
@@ -11,11 +11,45 @@
 </style>
 
 
-<div class="boite">
-    <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
-    </p>
+<div class="boitePrincipale">
+        <div id="accueil-header">
+            <p>Maison Principale    </p>
+        </div>
+
+        <div class="boite">
+
+            <div class="cafetiere-header">
+                <p>Cafetière n°1 </p>
+            </div>
+            
+            <button class="boutonInstant">
+                <p>CAFE INSTANTANNE<p> 
+            </button>
+
+            <div classe="Recap">
+                <p> Etat machine : </p>
+                <p> Temps utilisation : </p>
+                <p> Machine en marche : </p>
+                <p> Capsules Restantes : </p> 
+            </div>
+        </div>
+
+        <div class="boite">
+
+            <div class="cafetiere-header">
+                <p>Cafetière n°2 </p>
+            </div>
+            
+            <button class="boutonInstant">
+                <p>CAFE INSTANTANNE<p> 
+            </button>
+
+            <div classe="Recap">
+                <p> Etat machine : </p>
+                <p> Temps utilisation : </p>
+                <p> Machine en marche : </p>
+                <p> Capsules Restantes : </p> 
+            </div>
+        </div>
+    
 </div>
-<div class="boite">
-    <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
-    </p>
-</div> 
