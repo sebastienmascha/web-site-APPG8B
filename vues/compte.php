ab2349caf01770030e2b34b58333faf07a2d2a38
olivier <olivier.condere@outlook.fr>
2019-03-18 18:13:39 +0100

    [compte]retouches

--- vues/compte.php
+++ vues/compte.php
@@ -9,35 +9,24 @@
 
 
     
-    <div class="boite">
+    
             
-            <form method="POST" action="traitement.php">
-                    <p> <label for="Email">Email :</label>
-                        <input type="email" name="Email" id="Email" required/>
-                    </p>
-                    <p> <label for="Prénom">Prénom :</label>
-                      <input type="text" name="Prénom" id="Prénom" required/>
-                  </p>
-                  <p> <label for="Préférence">Préférence :</label>
-                      <input type="text" name="Préférence" id="Préférence" required/>
-                  </p>
-                  <p> <label for="Heure">Heure de préparation :</label>
-                      <input type="text"  name="Heure" id="Heure" required/>
-                  </p>
-                  
-                  <p> 
-                      <button type="submit">Valider</button>  
-                  </p>
-      
-                </form>
-                
+    <div class="boite">
+    <form method="POST" action="traitement.php">
+        <input type="email" name="Email" id="Email" placeholder="Email" required/>
+        <input type="text" name="Prénom" id="Prénom" placeholder="Prénom" required/>
+        <input type="text" name="Préférence" id="Préférence" placeholder="Préférence" required/>
+        <input type="text"  name="Heure" id="Heure" placeholder="Heure" required/>
+        <input type="submit" value="Valider">
+    </form>
+    <p class="utilisateur">type d'utilisateur</p>
+    </div>
                 <figure>
                 <img src="img/icon.png" class="icon" alt="icon">
-                <figcaption class="légende">NAME_USER</figcaption>
+                <figcaption class="légende">NAME_USER  </figcaption>
                 </figure>
                 
-                <p class="utilisateur">type d'utilisateur</p>
-                </div>
+                
     
     
     
