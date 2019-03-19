e4c5d1cad6f097bcfef324f4ba374badfa1df023
Astrid <astrid.benoit1998@gmail.com>
2019-03-19 14:10:59 +0100

    [Referent] Structure page referent et maison

---
+++ vues/referent-profil.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+* Vue : accueil
+*/
+?>
+
+<style>
+<?php include "css/css_referent-profil.css"; ?>
+</style>
+<div class=boite>
+	<p>
+		<ul>Gaétan</ul>
+		
+		
+	</p>
+</div>
+<div class=boite>
+	<p>
+		<h5>Ajouter un compte</h5>
+
+	</p>
+
+	
+
+
