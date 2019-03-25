9be896cea8ade61de6a59deb61e093cd3179fa84
Augustin Simon <augussim@gmail.com>
2019-03-25 11:58:40 +0100

    Some changes

--- vues/referent.php
+++ vues/referent.php
@@ -15,7 +15,7 @@
    		<input  type="button" onclick="document.location.href='index.php?cible=utilisateurs&fonction=referent-profil';" value="Gestion des profils">
    	</div>
    	<div class="h4">
-   		<input type="button" onclick="document.location.href='index.php?cible=utilisateurs&fonction=referent-maison';" value="Maison">
+   		<input type="button" onclick="document.location.href='index.php?cible=utilisateurs&fonction=referent-residence';" value="Maison">
    	</div>
  
 </div>
