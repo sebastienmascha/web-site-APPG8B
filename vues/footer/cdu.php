5d97cbdecbea8cf5c04f16df670d7ca50075e28f
Augustin Simon <augussim@gmail.com>
2019-03-25 09:46:59 +0100

    [FAQ] [CDU] Mise en place de la faq et des cdu

--- vues/footer/cdu.php
+++ vues/footer/cdu.php
@@ -3,3 +3,189 @@
 * Vue : CDU
 */
 ?>
+
+
+<style>
+<?php include "css/css_autres.css"; ?>
+</style>
+
+<div class="boxCdu">
+<h2> Conditions générales d'utilisation</h2>
+
+
+<p>
+Les présentes conditions générales de vente (CGU) s’appliquent, sans restriction ni réserve à
+l’ensemble des services proposés par Caco sur son site internet www.caco.fr 
+</p>
+
+<br/> 
+
+<h3> ARTICLE 1: Objet </h3>
+
+
+<p> Les présentes « conditions générales d'utilisation » ont pour objet l'encadrement juridique des modalités de mise à disposition des services du site Caco et leur utilisation par « l'Utilisateur ».
+
+Les conditions générales d'utilisation doivent être acceptées par tout Utilisateur souhaitant accéder au site. Elles constituent le contrat entre le site et l'Utilisateur. L’accès au site par l’Utilisateur signifie son acceptation des présentes conditions générales d’utilisation.
+
+Éventuellement :
+
+En cas de non-acceptation des conditions générales d'utilisation stipulées dans le présent contrat, l'Utilisateur se doit de renoncer à l'accès des services proposés par le site.
+
+Caco se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes conditions générales d'utilisation.
+</p>
+
+<br/> 
+
+<h3> ARTICLE 2: Mentions légales </h3>
+
+<p> L'édition du site www.Caco.fr est assurée par la Société Caco SARL au capital de 1M € dont le siège social est situé à Paris.
+
+Caco est une société du groupe Caco SARL au capital de 1M € dont le siège social est situé a Paris.
+
+L'hébergeur du site Caco.fr est la Société Caco SARL au capital de 1M€ dont le siège social est situé à Paris.
+</p>
+
+<br/> 
+
+<h3> ARTICLE 3: Définitions </h3>
+
+<p>La présente clause a pour objet de définir les différents termes essentiels du contrat :
+<ul>
+	<li>
+
+Utilisateur : ce terme désigne toute personne qui utilise le site ou l'un des services proposés par le site.
+	<li>
+Contenu utilisateur : ce sont les données transmises par l'Utilisateur au sein du site.
+	<li>
+Membre : l'Utilisateur devient membre lorsqu'il est identifié sur le site.
+	<li>
+Identifiant et mot de passe : c'est l'ensemble des informations nécessaires à l'identification d'un Utilisateur sur le site. L'identifiant et le mot de passe permettent à l'Utilisateur d'accéder à des services réservés aux membres du site. Le mot de passe est confidentiel.
+
+</ul>
+</p>
+
+
+<br/> 
+
+<h3> ARTICLE 4: Accès aux services </h3>
+
+
+<p>Le site permet à l'Utilisateur un accès gratuit aux services suivants :
+
+<ul>
+	<li>
+articles d’information ;
+	<li>
+annonces classées ;
+	<li>
+mise en relation de personnes ;
+	<li>
+publication de commentaires / d’œuvres personnelles ;
+
+</ul>
+
+Le site est accessible gratuitement en tout lieu à tout Utilisateur ayant un accès à Internet. Tous les frais supportés par l'Utilisateur pour accéder au service (matériel informatique, logiciels, connexion Internet, etc.) sont à sa charge.
+
+Selon le cas :
+
+L’Utilisateur non membre n'a pas accès aux services réservés aux membres. Pour cela, il doit s'identifier à l'aide de son identifiant et de son mot de passe.
+
+Le site met en œuvre tous les moyens mis à sa disposition pour assurer un accès de qualité à ses services. L'obligation étant de moyens, le site ne s'engage pas à atteindre ce résultat.
+
+Tout événement dû à un cas de force majeure ayant pour conséquence un dysfonctionnement du réseau ou du serveur n'engage pas la responsabilité de Caco.
+
+L'accès aux services du site peut à tout moment faire l'objet d'une interruption, d'une suspension, d'une modification sans préavis pour une maintenance ou pour tout autre cas. L'Utilisateur s'oblige à ne réclamer aucune indemnisation suite à l'interruption, à la suspension ou à la modification du présent contrat.
+
+L'Utilisateur a la possibilité de contacter le site par messagerie électronique à l’adresse contact@caco.com .
+</p>
+
+<br/> 
+
+<h3> ARTICLE 5: Propriété intellectuelle </h3>
+
+
+<p> Les marques, logos, signes et tout autre contenu du site font l'objet d'une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d'auteur.
+
+L'Utilisateur sollicite l'autorisation préalable du site pour toute reproduction, publication, copie des différents contenus.
+
+L'Utilisateur s'engage à une utilisation des contenus du site dans un cadre strictement privé. Une utilisation des contenus à des fins commerciales est strictement interdite.
+
+Tout contenu mis en ligne par l'Utilisateur est de sa seule responsabilité. L'Utilisateur s'engage à ne pas mettre en ligne de contenus pouvant porter atteinte aux intérêts de tierces personnes. Tout recours en justice engagé par un tiers lésé contre le site sera pris en charge par l'Utilisateur.
+
+Le contenu de l'Utilisateur peut être à tout moment et pour n'importe quelle raison supprimé ou modifié par le site. L'Utilisateur ne reçoit aucune justification et notification préalablement à la suppression ou à la modification du contenu Utilisateur.
+</p>
+
+
+<br/> 
+
+<h3> ARTICLE 6: Données personnelles </h3>
+
+
+<p>Les informations demandées à l’inscription au site sont nécessaires et obligatoires pour la création du compte de l'Utilisateur. En particulier, l'adresse électronique pourra être utilisée par le site pour l'administration, la gestion et l'animation du service.
+
+Le site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés. Le site est déclaré à la CNIL sous le numéro [numéro].
+
+En vertu des articles 39 et 40 de la loi en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit via :
+<ul>
+	<li>
+son espace personnel ;
+	<li>
+un formulaire de contact ;
+	<li>
+par mail à contact@caco.com ;
+	<li>
+par voie postale à Paris.
+</p>
+</ul>
+
+<br/> 
+
+<h3> ARTICLE 7: Responsabilité et force majeure </h3>
+
+
+<p> Les sources des informations diffusées sur le site sont réputées fiables. Toutefois, le site se réserve la faculté d'une non-garantie de la fiabilité des sources. Les informations données sur le site le sont à titre purement informatif. Ainsi, l'Utilisateur assume seul l'entière responsabilité de l'utilisation des informations et contenus du présent site.
+
+L'Utilisateur s'assure de garder son mot de passe secret. Toute divulgation du mot de passe, quelle que soit sa forme, est interdite.
+
+L'Utilisateur assume les risques liés à l'utilisation de son identifiant et mot de passe. Le site décline toute responsabilité.
+
+Tout usage du service par l'Utilisateur ayant directement ou indirectement pour conséquence des dommages doit faire l'objet d'une indemnisation au profit du site.
+
+Une garantie optimale de la sécurité et de la confidentialité des données transmises n'est pas assurée par le site. Toutefois, le site s'engage à mettre en œuvre tous les moyens nécessaires afin de garantir au mieux la sécurité et la confidentialité des données.
+
+La responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.
+</p>
+<br/> 
+
+<h3> ARTICLE 8: Liens hypertextes </h3>
+
+
+<p> De nombreux liens hypertextes sortants sont présents sur le site, cependant les pages web où mènent ces liens n'engagent en rien la responsabilité de www.caco.fr qui n'a pas le contrôle de ces liens.
+
+L'Utilisateur s'interdit donc à engager la responsabilité du site concernant le contenu et les ressources relatives à ces liens hypertextes sortants.
+</p>
+<br/> 
+
+<h3> ARTICLE 9: Évolution du contrat</h3>
+
+
+<p>Le site se réserve à tout moment le droit de modifier les clauses stipulées dans le présent contrat.
+</p>
+<br/> 
+
+<h3> ARTICLE 10: Durée </h3>
+
+
+<p> La durée du présent contrat est indéterminée. Le contrat produit ses effets à l'égard de l'Utilisateur à compter de l'utilisation du service.
+</p>
+<br/> 
+
+<h3> ARTICLE 11: Droit applicable et juridiction compétente</h3>
+
+
+<p> La législation française s'applique au présent contrat. En cas d'absence de résolution amiable d'un litige né entre les parties, seuls les tribunaux du ressort de la Cour d'appel de Paris sont compétents.
+</p>
+
+<br/> 
+
+</div>
--- vues/footer/faq.php
+++ vues/footer/faq.php
@@ -3,3 +3,70 @@
 * Vue : FAQ
 */
 ?>
+
+<style>
+<?php include "css/css_autres.css"; ?>
+</style>
+
+
+<div class="boiteFaq"> 
+	<h2> Question1 </h2> 
+
+	<ul>
+		<li>
+			<p> rep1 </p> 
+		</li>
+	</ul>
+</div>
+
+
+<br/>
+
+
+<div class="boiteFaq"> 
+	<h2> Question2 </h2> 
+	<ul>
+		<li>
+			<p> rep2 </p> 
+		</li>
+	</ul>
+</div>
+
+<br/>
+
+
+<div class="boiteFaq"> 
+	<h2> Question3 </h2> 
+	<ul>
+		<li>
+			<p> rep3 </p> 
+		</li>
+	</ul>
+</div>
+<br/>
+
+
+<div class="boiteFaq"> 
+	<h2> Question4 </h2> 
+	<ul>
+		<li>
+			<p> rep4 </p> 
+		</li>
+	</ul>
+</div>
+<br/>
+
+
+<div class="boiteFaq"> 
+	<h2> Question5 </h2> 
+	<ul>
+		<li>
+			<p> rep5 </p> 
+		</li>
+	</ul>
+</div>
+
+<br> 
+<div class="boiteFaq">
+Vous n'avez pas trouvé de réponse a votre question? N'hésitez pas a nous contacter <a href="index.php?cible=connexion&fonction=connexion" >  ici  </a>
+</div>
