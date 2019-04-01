d971a266b18123ebfb38630d3f4618ae0d3946cf
seb <sebastien.mascha@icloud.com>
2019-04-01 10:06:46 +0200

    [Footer] some changes

--- vues/footer/footer.php
+++ vues/footer/footer.php
@@ -16,29 +16,29 @@
 <footer>
     <div style="background-color:rgba(0,0,0,0.2); margin: 20px">
         <div>
-            <span> <a href="index.php?cible=connexion&fonction=connexion"> Contact</a> </span>
+            <span> <a href="index.php?cible=connexion&fonction=contact"> Contact</a> </span>
         </div>
 
         <table>
-            <tr>
-                <th class="thTable"><td width="33%"> <span> <a href="index.php?cible=utilisateurs&fonction=faq">FAQ</a> </span></th>
-                <th class="thTable"><td width="33%"> <span> <a href="index.php?cible=utilisateurs&fonction=cdu">CDU</a> </span></th>
-                <th class="thTable"><td width="33%"> <span> <a href="index.php?cible=utilisateurs&fonction=sav">SAV</a> </span></th>
+            <tr >
+                <th ><td width="33%"> <span> <a href="index.php?cible=connexion&fonction=faq">FAQ</a> </span></th>
+                <th ><td width="33%"> <span> <a href="index.php?cible=connexion&fonction=cdu">CDU</a> </span></th>
+                <th ><td width="33%"> <span> <a href="index.php?cible=connexion&fonction=sav">SAV</a> </span></th>
             </tr>
-            <tr>
-                <td class="tdTable" ><span class="spanTable"><td width="33%">FOIRE AUX QUESTIONS</span> </td>
-                <td class="tdTable"> <span class="spanTable"><td width="33%">CONDITIONS D'UTILISATION</span> </td>
-                <td class="tdTable"> <span class="spanTable"> <td width="33%">TECHNICIENS À VOTRE SERVICE</span></td>
+            <tr >
+                <td ><span ><td width="33%">FOIRE AUX QUESTIONS</span> </td>
+                <td > <span><td width="33%">CONDITIONS D'UTILISATION</span> </td>
+                <td > <span> <td width="33%">TECHNICIENS À VOTRE SERVICE</span></td>
             </tr>
-            <tr>
-                <td class="tdTable" ><span class="spanTable"> <td width="33%">Une question ?</span></td>
-                <td class="tdTable" ><span class="spanTable"><td width="33%"> Retrouvez l'ensemble des conditions d'utilisations certifiés par l'utilisateur.</span></td>
-                <td class="tdTable" ><span class="spanTable"><td width="33%"> Il vous suffit de contacter l'un de nos techniciens par téléphone au 0978 970 970 (prix d'un appel local) ou de vous présenter au comptoir SAV du magasin CaCo le plus proche de chez vous. La seule condition à remplir pour être pris en charge, est que la marque de l'appareil soit commercialisée chez CaCo.</span></td>
+            <tr >
+                <td ><span> <td width="33%">Une question ?</span></td>
+                <td ><span><td width="33%"> Retrouvez l'ensemble des conditions d'utilisations certifiés par l'utilisateur.</span></td>
+                <td ><span><td width="33%"> Il vous suffit de contacter l'un de nos techniciens par téléphone au 0978 970 970 (prix d'un appel local) ou de vous présenter au comptoir SAV du magasin CaCo le plus proche de chez vous. La seule condition à remplir pour être pris en charge, est que la marque de l'appareil soit commercialisée chez CaCo.</span></td>
             </tr>
         </table>
     </div>
 
-    <div style="height: 40px; text-align: center;margin: 35px;">
+    <div style="height: 40px; text-align: center;margin: 35px; font-size:30px">
         <span> © 2019 CaCo</span> <span style="font-size:20px;vertical-align: middle;">by Domisep</span>
     </div>
 
---
+++ vues/header2.php
@@ -0,0 +1,76 @@
+<?php
+/**
+* Vue : entête HTML
+*/
+?>
+
+<!doctype html>
+
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+
+    <title><?php echo $title; ?></title>
+    <meta name="description" content="CaCo, la cafetière connectée.">
+    <meta name="author" content="CaCo">
+
+    <link rel="stylesheet" href="css/css_connexion.css">
+    <style>
+        form {
+            width: 100%;
+        }
+
+        label {
+            display: inline-block;
+            width: 40%;
+        }
+
+        input[type=text],
+        input[type=password],
+        input[type=email],
+        select,
+        textarea {
+            width: 50%;
+            margin: 5px;
+            padding: 5px;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            height: 25px;
+            margin: 14px;
+        }
+
+        input[type=submit] {
+            background-color: #4CAF50;
+            color: white;
+            padding: 12px 20px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            margin: 20px;
+        }
+
+        input[type=submit]:hover {
+            background-color: #45a049;
+        }
+    </style>
+</head>
+
+<body>
+
+
+    <div id="conteneur">
+
+
+
+        <div class="wrap">
+            <main>
+                <div id="page-wrap" class="bg-trans">
+
+                    <div id="page-header">
+                        <img src="img/home.png" alt="intitulé img" class="icon-page" />
+                        <h1><?php echo $title; ?></h1>
+                    </div>
+
+                    
