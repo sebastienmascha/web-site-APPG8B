b062a666a99db49d9d96e503505dce4260ec9407
Agathe Le Bris <agathelebris98@gmail.com>
2019-05-20 12:05:32 +0200

    [page capteurs] ajout info capteurs

--- vues/fr/referent-capteur.php
+++ vues/fr/referent-capteur.php
@@ -8,27 +8,64 @@
 </style>
 
 <?php foreach ($capteurs as $capteur) { ?>
-	<div class="boite" style="background:url('img/Micro.jpg'); background-size:cover; ">
+	<div class="boite"  >
 
 		<p class="afficherTitre"> 
 			<?php echo $capteur['type']; ?>
 		</p>
+		<p >
+			<?php
+			if ($capteur['type']=='Température') {
+				echo 'Utilisation : Récupère la température de la boisson pour savoir s il faut le rechauffer <br/>';
+				echo 'Etat : ';
+				if ($capteur['etat']==1) {
+					echo 'Bon <br/>';
+				} else {
+					echo 'Critique<br/>';
+				}
+				echo 'Température mesurée actuellement : ';
+			} else if ($capteur['type']=='Présence de tasse'){
+				echo 'Utilisation : Permet de savoir si il y a une tasse devant la cafetière <br/>';
+				echo 'Etat : ';
+				if ($capteur['etat']==1) {
+					echo 'Bon<br/>';
+				} else {
+					echo 'Critique<br/>';
+				}
+				echo 'Distance calculée : ';
+			} else if ($capteur['type']=='Présence de capsules'){
+				echo 'Utilisation : Permet de savoir si il reste des capsules dans la machine <br/>';
+				echo 'Etat : ';
+				if ($capteur['etat']==1) {
+					echo 'Bon<br/>';
+				} else {
+					echo 'Critique<br/>';
+				}
+				echo 'Distance calculée : ';
+			}else if ($capteur['type']=='Sonore'){
+				echo 'Utilisation : Permet de commander un café avec la voix <br/>';
+				echo 'Etat : ';
+				if ($capteur['etat']==1) {
+						echo 'Bon<br/>';
+				} else {
+						echo 'Critique<br/>';
+				}
+			} else if ($capteur['type']=='Résistance Chauffante'){
+				echo 'Utilisation : Permet de réchauffer le café si besoin <br/>';
+				echo 'Etat : ';
+				if ($capteur['etat']==1) {
+						echo 'Bon<br/>';
+				} else {
+						echo 'Critique<br/>';
+				}
+				
+			}
+			?>
+		</p>
+	
 
-		<div class="center">
-			<p class="button">
-				<input  type="button" value="Afficher Infos" onclick="edit_div('InfosCapteurs');" />
-			</p>
-              
-        </div>
-		
-		<div id="editer" style="display:none">
-			<p> Utilisation : </p>
-			<p> Etat : </p>
-
-		</div>
-
-
-
+				
 
 	</div>
+
 <?php } ?>
