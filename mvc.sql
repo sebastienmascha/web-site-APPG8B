a9090a551f97f68c22236123237ed90702b44b15
seb <sebastien.mascha@icloud.com>
2019-05-25 16:53:47 +0200

    [BDD] add house for invite

--- mvc.sql
+++ mvc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: May 25, 2019 at 02:41 PM
+-- Generation Time: May 25, 2019 at 02:53 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -240,7 +240,9 @@ INSERT INTO `users_homes` (`id`, `idMaison`, `idUser`) VALUES
 (10, 2, 5),
 (11, 1, 6),
 (12, 2, 6),
-(13, 1, 3);
+(13, 1, 3),
+(14, 1, 8),
+(15, 2, 8);
 
 -- --------------------------------------------------------
 
@@ -440,7 +442,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users_homes`
 --
 ALTER TABLE `users_homes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `users_main`
